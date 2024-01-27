f92f98768458c377e071bf5cc8ea5eb38ea9aa0a
ahkubek <120653318+ahkubek@users.noreply.github.com>
2024-01-27 09:10:02 +0300

    Car

---
+++ src/Car.java
@@ -0,0 +1,21 @@
+public class Car {
+    public String model;
+    public String color;
+
+    // Создаем параметризованный конструктор
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+
+        // Печатаем значения атрибутов Car
+        System.out.println("Model: " + this.model);
+        System.out.println("Color: " + this.color);
+    }
+
+    public static void main(String[] args) {
+        // Создаем новый объект Car;
+        // Обязательно задаем ему значения атрибутов в том же порядке,
+        // в котором они заданы в конструкторе
+        Car myCar = new Car("Toyota", "Red");
+    }
+}
